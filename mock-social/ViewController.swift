64d660e3c0626c0f52001d5b3b3d641fe8dfca64
Tania Nemeth <tnemeth@pipeline.sbcc.edu>
2019-04-02 10:34:54 -0700

    Initial Commit

---
+++ mock-social/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mock-social
+//
+//  Created by tania on 4/2/19.
+//  Copyright © 2019 tania nemeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
